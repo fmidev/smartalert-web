25fafdd59a413d8fdc719524388189cb9e07c2d8
Mikko Rauhala <mikko@rauhala.net>
2016-10-25 10:15:43 +1200

    Initial version

---
+++ list.php
@@ -0,0 +1,21 @@
+<?php
+$DIR=trim(`find data/publishedCap -type d|sort -n|tail -1`);
+
+$FILES = scandir($DIR);
+
+$capfiles;
+
+foreach ($FILES as $file)
+  {
+    if (preg_match("/_ALERT_/",$file))
+      {
+	$capfiles[]=$DIR."/".$file;
+      }
+  }
+
+header("Content-type: application/json");
+header("Pragma: no-cache");
+header("Cache-control: no-cache, must-revalidate");
+header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
+print json_encode($capfiles);
+
