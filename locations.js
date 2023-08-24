f23ca8337e540a787f46d29bc5c0792e26a4f752
Joni Häkkänen <hakkanen@dev.dev.weatherproof.fi>
2023-08-24 15:46:23 +0300

    emply location.js file

--- locations.js
+++ locations.js
@@ -9,23 +9,21 @@ var locations = [
       ]
     },
     {
-      minZoom: 9,
-      maxZoom: 15,
-      fillColor: 'red',
+      minZoom: 1, //Number from 1 to 18.
+      maxZoom: 18, //Number from 1 to 18.
+      fillColor: '#ff0000', //Any color string. To disable markers, remove this line or use empty string / null.
       locations: [
-        { name: "Kitee", lon: 30.1, lat: 62.5 },
-        { name: "mantsala", lon: 25.2, lat: 60.2 },
-        { name: "Juupajoki", lon: 25.3, lat: 60.3 },
-        { name: "salla", lon: 25.6, lat: 65.4 }
+        { name: "example", lon: null, lat: null }, //replace name with display name and add coordinates
+        { name: 'example', lon: null, lat: null },
       ]
     },
     {
-      minZoom: 1,
-      maxZoom: 15,
-      fillColor: 'green',
+      minZoom: 1, //Number from 1 to 18.
+      maxZoom: 18, //Number from 1 to 18.
+      fillColor: '#ff0000', //Any color string. To disable markers, remove this line or use empty string / null.
       locations: [
-        { name: "Paijanne", lon: 25.2, lat: 60.3 },
-        { name: "Nasijarvi", lon: 25.1, lat: 60.4 }
+        { name: "example", lon: null, lat: null }, //replace name with display name and add coordinates
+        { name: 'example', lon: null, lat: null },
       ]
-    }
+    },
 ]
