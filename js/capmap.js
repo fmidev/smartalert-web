94f6753040c2abe5eaf82be33c84552ba00f84d5
Joni Häkkänen <hakkanen@dev.dev.weatherproof.fi>
2024-11-26 13:00:58 +0200

    [PAK-3494] Fix UZ translations

--- js/capmap.js
+++ js/capmap.js
@@ -689,6 +689,7 @@ function DayControl(controlDiv) {
 
     setDay1UI.appendChild(dayTextElement)
     controlDiv.appendChild(setDay1UI)
+    selectedDAY === nul0l && setActiveButton(setDay1UI)
     setEventListener(setDay1UI, 1, 'Show events for tomorrow.')
   }
 
